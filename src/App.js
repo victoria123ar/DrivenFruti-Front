40bd53b3acc628f29f39f6fafc0fea88a91f5d5b
crixstn <crixstn@gmail.com>
2023-01-24 19:25:31 -0300

    initial layout - signUp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
+import SignUpPage from "./pages/SignUpPage/SignUpPage"
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Home />} path="/" />
+        <Route element={<SignUpPage /> } path="sign-up" />
       </Routes>
     </>
   )
---
+++ src/pages/SignUpPage/SignUpPage.js
@@ -0,0 +1,50 @@
+import { ContainerInputs, SignUpLayout, Container} from "./styled";
+import logo from "../../assets/logos/bigLogoSticker.png"
+
+export default function SignUpPage(){
+    return(
+        <SignUpLayout>
+            <Container>
+                <img src={logo}/>
+                
+                <ContainerInputs>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            name="name"
+                        />
+
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                        />
+
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            name="password"
+                        />
+
+                        <input
+                            type="password"
+                            placeholder="Confirme sua senha"
+                            name="ConfirmPassword"
+                        />
+
+                        <button>
+                            <ion-icon name="log-in-outline"></ion-icon>
+                            <p>Cadastrar</p>
+                        </button>
+                    </form>
+
+                    <button>
+                        <ion-icon name="person-outline"></ion-icon>
+                        <p>Já tenho uma conta!</p>
+                    </button>
+                </ContainerInputs>
+            </Container>
+        </SignUpLayout>
+    )
+}
