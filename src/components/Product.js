257e3ebf1e6df79f4633b20dbd903a76e69382d0
Danilo Leão <olin_ad@hotmail.com>
2023-01-24 16:09:11 -0300

    home

---
+++ src/components/Product.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+function Product({ product }) {
+  const {
+    name,
+    price,
+    thumbnail,
+    category,
+    productId,
+  } = product;
+
+  console.log(thumbnail)
+
+  return (
+    <StyledProduct>
+      <figure>
+        <img alt="product" src={`./images/${thumbnail}`} />
+      </figure>
+      <div>
+        <p>{name}</p>
+        <p>{`R$ ${price.toFixed(2)}`}</p>
+      </div>
+    </StyledProduct>
+  )
+};
+
+export default Product;
+
+const StyledProduct = styled.div`
+  /* background-color: red; */
+  border: 1px solid rgba(0, 0, 0, 0.2 );
+  border-radius: 12px;
+  width: 200px;
+  height: 200px;
+  margin: 15px;
+  padding: 18px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+
+  figure {
+    /* background-color: purple; */
+    width: 100%;
+    height: 70%;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    /* background-color: yellow; */
+  }
+
+  & > div {
+    /* background-color: purple; */
+    height: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,147 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Product from "../components/Product";
+
+function Home() {
+  const [search, setSearch] = useState('');
+  const [products, setProducts] = useState([]);
+
+  function handleSearch(target) {
+    const { value } = target;
+    setSearch(value);
+  };
+
+  useEffect(() => {
+    const URL = 'http://localhost:5000';
+
+    const fetcher = async () => {
+      const productsData = await axios.post(URL, {});
+      const { data } = productsData;
+
+      setProducts(data);
+    }
+    fetcher();
+
+    return () => {
+      console.log('Cleaning...')
+    };
+  }, []);
+
+  return (
+    <div>
+      <StyledHeader>
+        <div>
+          <figure>
+            <img alt="logo" src="" />
+          </figure>
+          <div>
+            <ion-icon name="cart-outline"></ion-icon>
+            <ion-icon name="log-in-outline"></ion-icon>
+          </div>
+        </div>
+        <input
+          type="text"
+          value={search}
+          onChange={({ target }) => handleSearch(target)}
+          placeholder="Qual produto você procura?"
+        >
+        </input>
+        <div />
+      </StyledHeader>
+      <StyledMain>
+        <div>
+          <ul>
+            <li>Frutas</li>
+            <li>Verduras</li>
+            <li>Legumes</li>
+            <li>Padaria</li>
+            <li>Orgânicos</li>
+            <li>Bebidas</li>
+          </ul>
+        </div>
+        <div>
+          {products.map((product) => <Product product={product} />)}
+        </div>
+      </StyledMain>
+    </div>
+  );
+};
+
+const StyledHeader = styled.header`
+  /* background-color: #f9f9f9; */
+  position: relative;
+  position: fixed;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  
+  & > div:first-of-type {
+    /* background-color: green; */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    & > div {
+    /* background-color: red; */
+    font-size: 25px;
+
+    * {
+      margin: 8px;
+    }
+    }
+  }
+
+  input {
+    /* background-color: red; */
+    padding: 7px;
+    border: 1px solid red;
+    border-left-width: 4px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    top: 50px;
+    width: 96%;
+  }
+
+  & > div:nth-of-type(2) {
+    min-width: 100%;
+    min-height: 100px;
+    position: absolute;
+    filter: blur(2px);
+    background-color: rgba(255, 255, 255, 0.8);
+    z-index: -2;
+  }
+`;
+
+const StyledMain = styled.main`
+  /* background-color: green; */
+  position: relative;
+  top: 100px;
+  z-index: -3;
+
+  & > div:first-of-type {
+    ul {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      list-style-type: none;
+      padding: 7px;
+
+      li {
+        margin: 8px;
+      }
+    }
+  }
+
+  & > div:nth-of-type(2) {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+`;
+
+export default Home;
